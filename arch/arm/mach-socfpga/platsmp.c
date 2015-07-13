torvalds/linux

arm: socfpga: Change cpu1 reset register for Arria 10 SOC
The Arria 10 SOC had a different cpu1 reset register than the Cyclone5 and
the Arria5 SOC.

Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>

arch/arm/mach-socfpga/platsmp.c
@@ -48,7 +48,10 @@ static int __cpuinit socfpga_boot_secondary(unsigned int cpu, struct task_struct
 		outer_clean_range(0, trampoline_size);
 
 		/* This will release CPU #1 out of reset.*/
-		__raw_writel(0, rst_manager_base_addr + 0x10);
+		if (of_machine_is_compatible("altr,socfpga-arria10"))
+			__raw_writel(0, rst_manager_base_addr + 0x20);
+		else
+			__raw_writel(0, rst_manager_base_addr + 0x10);
 	}
 
 	return 0;

xaxaxa/linux-socfpga

Conflicts:
	arch/arm/mach-socfpga/platsmp.c

arch/arm/mach-socfpga/platsmp.c
@@ -47,8 +47,11 @@ static int socfpga_boot_secondary(unsigned int cpu, struct task_struct *idle)
 		smp_wmb();
 		outer_clean_range(0, trampoline_size);
 
-		/* This will release CPU #1 out of reset. */
-		writel(0, rst_manager_base_addr + SOCFPGA_RSTMGR_MODMPURST);
+		/* This will release CPU #1 out of reset.*/
+		if (of_machine_is_compatible("altr,socfpga-arria10"))
+			writel(0, rst_manager_base_addr + 0x20);
+		else
+			writel(0, rst_manager_base_addr + 0x10);
 	}
 
 	return 0;